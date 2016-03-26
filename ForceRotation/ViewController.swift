e55ebfdb46c5908ac9565bc4119cf7bd5401eeae
hayashidakazumi <axhfaro@gmail.com>
2016-03-26 14:23:58 +0900

    Initialize

---
+++ ForceRotation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForceRotation
+//
+//  Created by HayashidaKazumi on 2016/03/26.
+//  Copyright © 2016年 HayashidaKazumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
