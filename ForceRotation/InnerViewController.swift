82cfb238e5deff4c29c928bd4a818bcd0af61dcb
hayashidakazumi <axhfaro@gmail.com>
2016-03-26 14:41:58 +0900

    Init layout

---
+++ ForceRotation/InnerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  InnerViewController.swift
+//  ForceRotation
+//
+//  Created by HayashidaKazumi on 2016/03/26.
+//  Copyright © 2016年 HayashidaKazumi. All rights reserved.
+//
+
+import UIKit
+
+class InnerViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
